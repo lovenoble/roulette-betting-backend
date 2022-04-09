0b19903f1f14ab78e1570294528712cf19f17989
t3rl <t3rl@protonmail.com>
2022-04-09 14:39:01 -0500

    update store config to handle prod MONGO_CONNECTION_STRING

--- server/store/config/index.ts
+++ server/store/config/index.ts
@@ -4,6 +4,22 @@ const {
     MONGO_HOST,
     MONGO_PORT,
     MONGO_DATABASE_NAME,
+    MONGO_CONNECTION_STRING
 } = process.env
 
-export const storeUri = MONGO_HOST ? `mongodb://${MONGO_ROOT_USERNAME}:${MONGO_ROOT_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE_NAME}` : ''
+const getStoreUri = () => {
+  // Connecting to stage/pre/prod requires a different format for the connection string.
+  // It should be specified in the MONGO_CONNECTION_STRING environment variable.
+  if (MONGO_CONNECTION_STRING) {
+      return MONGO_CONNECTION_STRING
+  }
+
+  if (!MONGO_HOST) {
+      return ''
+  }
+
+  return `mongodb://${MONGO_ROOT_USERNAME}:${MONGO_ROOT_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}` + 
+    `/${MONGO_DATABASE_NAME}`
+}
+
+export const storeUri = getStoreUri()
