e777d6a88a70542874c00ae4707ecad446cd4e42
hierophant <hierophanticphenom@protonmail.com>
2022-06-16 11:57:20 -0500

    reconnect logic pseudo code

--- server/store/listener/index.ts
+++ server/store/listener/index.ts
@@ -29,7 +29,17 @@ export default class SmartContractListener {
 		await this.service.gameMode.ensureGameModes()
 	}
 
+	/*  
+
+	async function reconnect(){
+  // connect w/ services 
+	}
+	*/
+
+	// look up connect and disconnect logic and abstract into function
+	// smart contracts get mounted here, if connection is lost, will not attempt to reconnect
 	async start() {
+		// should only occur on start, do not call again if reconnect needed
 		await this.beforeStart()
 
 		try {
@@ -49,6 +59,23 @@ export default class SmartContractListener {
 			// spinAPI.contract.on(EventNames.RandomNumberRequested, (...args) => console.log(args))
 		} catch (err) {
 			logger.error(err)
+
+			/*
+			let isDisconnected = await service0.isDisconnected() || await service1.isDisconnected() || await service2.isDisconnected() || await service3.isDisconnected()
+			let reconnectAttempts = 10
+			
+			if(isDisconnected){
+				while(reconnectAttempts > 0 && isDisconnected){
+					await setTimeout(10000){
+						await reconnect()
+					}
+				}
+				
+				reconnectAttempts -= 1
+
+				isDisconnected = await service0.isDisconnected() || await service1.isDisconnected() || await service2.isDisconnected() || await service3.isDisconnected()
+			}
+			*/
 		}
 	}
 
