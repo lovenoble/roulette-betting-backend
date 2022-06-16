9a4a8d2f8f2b56fe5f84e8421ca64b776540e9ac
hierophant <hierophanticphenom@protonmail.com>
2022-06-16 14:26:43 -0500

    refactored reconnect() to core logic

--- server/store/listener/index.ts
+++ server/store/listener/index.ts
@@ -37,58 +37,19 @@ export default class SmartContractListener {
 		await this.stop()
 
 		while (reconnectAttempts > 0 && fareApiListeners + spinApiListeners < 5) {
-			if (fareApiListeners + spinApiListeners === 2) {
-				fareAPI.contract.removeAllListeners()
-				spinAPI.contract.removeAllListeners()
+			logger.warn('Attempting to reconenct...')
 
-				logger.warn('Attempting to reconenct...')
+			fareAPI.contract.removeAllListeners()
+			spinAPI.contract.removeAllListeners()
 
-				fareAPI.contract.on(EventNames.Transfer, this.listeners.fareTransfer)
-				spinAPI.contract.on(EventNames.GameModeUpdated, this.listeners.gameModeUpdated)
-				spinAPI.contract.on(EventNames.EntrySubmitted, this.listeners.entrySubmitted)
-				// spinAPI.contract.on(EventNames.RoundConcluded, this.listeners.roundConcluded)
-				// spinAPI.contract.on(EventNames.EntrySettled, this.listeners.entrySettled)
+			fareAPI.contract.on(EventNames.Transfer, this.listeners.fareTransfer)
+			spinAPI.contract.on(EventNames.GameModeUpdated, this.listeners.gameModeUpdated)
+			spinAPI.contract.on(EventNames.EntrySubmitted, this.listeners.entrySubmitted)
+			spinAPI.contract.on(EventNames.RoundConcluded, this.listeners.roundConcluded)
+			spinAPI.contract.on(EventNames.EntrySettled, this.listeners.entrySettled)
 
-				fareApiListeners = fareAPI.contract.listenerCount()
-				spinApiListeners = spinAPI.contract.listenerCount()
-
-				logger.info(`fareAPI.contract.listeners(): ${fareApiListeners}`)
-				logger.info(`spinAPI.contract.listeners(): ${spinApiListeners}`)
-			} else if (fareApiListeners + spinApiListeners === 3) {
-				fareAPI.contract.removeAllListeners()
-				spinAPI.contract.removeAllListeners()
-
-				logger.warn('Attempting to reconenct...')
-
-				fareAPI.contract.on(EventNames.Transfer, this.listeners.fareTransfer)
-				spinAPI.contract.on(EventNames.GameModeUpdated, this.listeners.gameModeUpdated)
-				spinAPI.contract.on(EventNames.EntrySubmitted, this.listeners.entrySubmitted)
-				spinAPI.contract.on(EventNames.RoundConcluded, this.listeners.roundConcluded)
-				// spinAPI.contract.on(EventNames.EntrySettled, this.listeners.entrySettled)
-
-				fareApiListeners = fareAPI.contract.listenerCount()
-				spinApiListeners = spinAPI.contract.listenerCount()
-
-				logger.info(`fareAPI.contract.listeners(): ${fareApiListeners}`)
-				logger.info(`spinAPI.contract.listeners(): ${spinApiListeners}`)
-			} else if (fareApiListeners + spinApiListeners === 4) {
-				fareAPI.contract.removeAllListeners()
-				spinAPI.contract.removeAllListeners()
-
-				logger.warn('Attempting to reconenct...')
-
-				fareAPI.contract.on(EventNames.Transfer, this.listeners.fareTransfer)
-				spinAPI.contract.on(EventNames.GameModeUpdated, this.listeners.gameModeUpdated)
-				spinAPI.contract.on(EventNames.EntrySubmitted, this.listeners.entrySubmitted)
-				spinAPI.contract.on(EventNames.RoundConcluded, this.listeners.roundConcluded)
-				spinAPI.contract.on(EventNames.EntrySettled, this.listeners.entrySettled)
-
-				fareApiListeners = fareAPI.contract.listenerCount()
-				spinApiListeners = spinAPI.contract.listenerCount()
-
-				logger.info(`fareAPI.contract.listeners(): ${fareApiListeners}`)
-				logger.info(`spinAPI.contract.listeners(): ${spinApiListeners}`)
-			}
+			fareApiListeners = fareAPI.contract.listenerCount()
+			spinApiListeners = spinAPI.contract.listenerCount()
 
 			reconnectAttempts -= 1
 		}
@@ -103,24 +64,15 @@ export default class SmartContractListener {
 
 			// Spin
 			spinAPI.contract.on(EventNames.GameModeUpdated, this.listeners.gameModeUpdated)
-			// spinAPI.contract.on(EventNames.EntrySubmitted, this.listeners.entrySubmitted)
-			// spinAPI.contract.on(EventNames.RoundConcluded, this.listeners.roundConcluded)
-			// spinAPI.contract.on(EventNames.EntrySettled, this.listeners.entrySettled)
+			spinAPI.contract.on(EventNames.EntrySubmitted, this.listeners.entrySubmitted)
+			spinAPI.contract.on(EventNames.RoundConcluded, this.listeners.roundConcluded)
+			spinAPI.contract.on(EventNames.EntrySettled, this.listeners.entrySettled)
 
 			// @NOTE: Need to implement NFTWon event
 			// spinAPI.contract.on(EventNames.NFTWon, this.listeners.nftWon)
 
 			// @NOTE: Perhaps this event won't be needed since we already get the random number from roundConcluded
 			// spinAPI.contract.on(EventNames.RandomNumberRequested, (...args) => console.log(args))
-
-			const fareApiListeners = fareAPI.contract.listenerCount()
-			const spinApiListeners = spinAPI.contract.listenerCount()
-			logger.info(`fareAPI.contract.listeners(): ${fareApiListeners}`)
-			logger.info(`spinAPI.contract.listeners(): ${spinApiListeners}`)
-
-			if (fareApiListeners + spinApiListeners < 5) {
-				await this.reconnect()
-			}
 		} catch (err) {
 			logger.error(err)
 			await this.reconnect()
