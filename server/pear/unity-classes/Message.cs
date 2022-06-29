920f730e1c16bef99c24a2f57fe3acfc2554bd7e
timraddish <timraddish@gmail.com>
2022-06-29 15:38:36 -0500

    Generated C# classes and removed .DS_Store

---
+++ server/pear/unity-classes/Message.cs
@@ -0,0 +1,29 @@
+// 
+// THIS FILE HAS BEEN GENERATED AUTOMATICALLY
+// DO NOT CHANGE IT MANUALLY UNLESS YOU KNOW WHAT YOU'RE DOING
+// 
+// GENERATED USING @colyseus/schema 1.0.34
+// 
+
+using Colyseus.Schema;
+
+public partial class Message : Schema {
+	[Type(0, "string")]
+	public string id = default(string);
+
+	[Type(1, "string")]
+	public string text = default(string);
+
+	[Type(2, "string")]
+	public string createdBy = default(string);
+
+	[Type(3, "string")]
+	public string username = default(string);
+
+	[Type(4, "string")]
+	public string colorTheme = default(string);
+
+	[Type(5, "number")]
+	public float timestamp = default(float);
+}
+
